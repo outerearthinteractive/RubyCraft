fd93211e1d51013445fa2df89d37e7265768581e
Rhys <rice@outerearth.net>
2011-10-25 12:08:09 -0700

    Updated config

--- config.rb
+++ config.rb
@@ -1,13 +1,9 @@
-require("../lib/plugin.rb")
-class ConfigPlugin < Plugin
-	attr_accessor :ip, :port, :maxplayers, :motd
-	def initialize(name, author, version, description, ip, port, maxplayers, motd)
-		super(name, author, version, description)
-		self.ip = ip
-		self.port = port
-		self.maxplayers = maxplayers
-		self.motd = motd
-		self.maxversion = 9000 #cur. ver. = ~20
-		self.minversion = 0
-	end
+require("./lib/plugin.rb")
+module Config
+	@ip = "127.0.0.1"
+	@port = 25565
+	@maxplayers = 20 # -1 for unlimted.
+	@motd = "Welcome to the server!"
+	@maxversion = 9000 #cur. ver. = ~20
+	@minversion = 0
 end
