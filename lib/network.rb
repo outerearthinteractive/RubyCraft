2181de97a641209a53611c640fe338883df53b1c
Tanner Danzey <arkaniad@gmail.com>
2011-10-26 18:17:49 -0500

    added 'quit' command. ctrl-c was annoying me.

--- lib/network.rb
+++ lib/network.rb
@@ -1,8 +1,14 @@
 module NetworkServer
 	def post_init
-	  puts "-- someone connected to the echo server!"
+	  	puts "-- someone connected to the echo server!"
 	end
 	def receive_data data
-	  send_data ">>> you sent: #{data}"
+		
+		if data =~ /quit/i then
+			send_data ">>> goodbye"
+			close_connection
+		else
+	 		send_data ">>> you sent: #{data}"
+		end
 	end
 end
