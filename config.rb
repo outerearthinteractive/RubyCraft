6163213a788b4c2cdd7c8ef5719d478f6e6707fd
Rhys <rice@outerearth.net>
2011-10-26 18:19:38 -0700

    Removed useless require from the Config File

--- config.rb
+++ config.rb
@@ -1,9 +1,10 @@
-require("./lib/plugin.rb")
-
 class Configuration
+	#This is a list of vars that can be accessed outside the class directly.
+	#Pretty much everything in this file should be listed here.
 	attr_accessor :ip, :port, :maxplayers, :motd, :maxversion, :minversion
 	
 	def initialize()
+		#All the variables & there values should be set here.
 		self.ip = "127.0.0.1"
 		self.port = 25565
 		self.maxplayers = 20
