9f1c738bdbaaefc58d85862b8082260c780cc9ac
Rhys <rice@outerearth.net>
2011-10-28 20:57:27 -0700

    Updated chunk and block

--- lib/block.rb
+++ lib/block.rb
@@ -1,9 +1,13 @@
 class Block
 @block_x
 @block_y
+@block_z
 @block_type
-@block_metadata
-	def initialize
-	
+@block_data
+	def initialize x y z type
+		@block_x = x
+		@block_y = y
+		@block_z = z
+		@block_type = type
 	end
 end
--- lib/chunk.rb
+++ lib/chunk.rb
@@ -1,8 +1,34 @@
+require("block.rb")
+
 class Chunk
+@server
+@world
 @chunk_x
 @chunk_y
 @chunk_blocks
-	def initialize
-		
+	def initialize server world
+		@server = server
+		@world = world
+		@chunk_blocks = []
+		x = 0
+		16.times do
+			z=0
+			16.times do
+				y=0
+				128.times do
+					@chunk_blocks += [Block.new(x,y,z,0)]
+				end
+			end
+		end
+	end
+	def get_block_at x y z
+		@chunk_blocks.each do |block|
+			if block.x==x&&block.y==y&block.z==z
+				return block
+			end
+		end
+		return nil
+	end
+	def save
 	end
 end
