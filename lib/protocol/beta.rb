a2b0f19666a425fa5a1217120970ba3eb8883441
Tanner Danzey <arkaniad@gmail.com>
2011-11-01 21:05:29 -0500

    Player handling is basically implemented. The packet just needs to be stripped of non-letter characters, which is easy in theory.

--- lib/protocol/beta.rb
+++ lib/protocol/beta.rb
@@ -103,7 +103,7 @@ class BetaProtocol
 	def handshake connection, packet
 	  	@log.info("Handshake from player: #{packet.chomp}")
 		@log.info("Adding player to active list.")
-		@players += {packet.chomp => Player.new(packet.chomp)}
+		@players[packet.chomp] = Player.new(packet.chomp)
 	  	payload =  [@packets[:handshake]]
 		payload.concat(string16 "-")
 		connection.send_data bisect(payload).pack("C*")
